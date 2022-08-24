f8052616934c704da786b555ab9e734d02fed35a
MOSKALCHUK <moskalchuk@MacBook-Air-MOSKALCHUK.local>
2022-08-24 21:15:39 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="block" :class="{animated: isAnimated}"></div>
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <transition>
+      <p v-if="isTextVisible">This is a temporary text</p>
+    </transition>
+    <button @click="toggleText">Toggle</button>
+  </div>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+</template>  
+
+<script>
+export default {
+  data() {
+    return { 
+      dialogIsVisible: false,
+      isAnimated: false,
+      isTextVisible: false
+      };
+  },
+  methods: {
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.isAnimated = true;
+    },
+    toggleText() {
+      this.isTextVisible = !this.isTextVisible;
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  transition: all 0.5s ease;
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+
+.animated {
+  transform: translateX(-150px);
+}
+</style>
